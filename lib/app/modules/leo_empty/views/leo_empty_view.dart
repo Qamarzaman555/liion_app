0e122a10dae2e32794378a8ef1ac3ec29681a471
Qamar Zaman <qamarxk12345@gmail.com>
2025-11-26 18:25:50 +0500

    moved leo connection code to separate branch

--- lib/app/modules/leo_empty/views/leo_empty_view.dart
+++ lib/app/modules/leo_empty/views/leo_empty_view.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import '../controllers/leo_home_controller.dart';
-
-class LeoHomeView extends GetView<LeoHomeController> {
-  const LeoHomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Leo Empty View')));
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import '../controllers/leo_home_controller.dart';
+//
+// class LeoHomeView extends GetView<LeoHomeController> {
+//   const LeoHomeView({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(body: Center(child: Text('Leo Empty View')));
+//   }
+// }
