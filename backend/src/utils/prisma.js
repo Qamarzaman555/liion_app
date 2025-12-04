4bd388d6f8b5e58b1b3b8eabce707a7fe329524c
Qamar Zaman <qamarxk12345@gmail.com>
2025-12-05 00:08:36 +0500

    removed: backend directory

--- backend/src/utils/prisma.js
+++
@@ -1,8 +0,0 @@
-import { createPrismaClient } from './prisma-timezone.js';
-
-/**
- * Shared Prisma client instance with Pakistani timezone support
- * All routes should import this instead of creating new PrismaClient instances
- */
-export const prisma = createPrismaClient();
-
