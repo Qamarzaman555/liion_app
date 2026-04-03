1498e206d05901d370f8a0ed1bf54a3b7bac0589
Qamar Zaman <qamarxk12345@gmail.com>
2026-04-03 12:14:57 +0500

    feat: update iOS project version to 64, change firmware storage path to 'Internal fw'

--- ios/Runner/BLEService.swift
+++ ios/Runner/BLEService.swift
@@ -175,9 +175,9 @@ class BLEService: NSObject {
     private lazy var firestore: Firestore = {
         return Firestore.firestore()
     }()
-    private let collectionName = "leoFilesProduction"
+    // private let collectionName = "leoFilesProduction"
     // private let collectionName = "leoFilesOpen"
-    // private let collectionName = "leoFilesInternal"
+    private let collectionName = "leoFilesInternal"
     
     // Connection state (matching Android STATE_DISCONNECTED, STATE_CONNECTING, STATE_CONNECTED)
     private enum ConnectionState {
